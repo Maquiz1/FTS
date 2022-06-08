52b435598a91a1a34de021d3e6cf262a350022d6
Maquiz1 <manquiz92@gmail.com>
2022-06-08 09:10:25 +0300

    fix boostraps

--- dashboard1.php
+++ dashboard1.php
@@ -496,11 +496,11 @@ $tv = $override->getCount('visit', 'visit_date', date('Y-m-d'));
                     </div>
                     <!-- /.row -->
                     <!-- Main row -->
-                    <div class="row">
+                    <!-- <div class="row"> -->
                         <!-- Left col -->
-                        <section class="col-lg-7 connectedSortable">
+                        <!-- <section class="col-lg-7 connectedSortable"> -->
                             <!-- Custom tabs (Charts with tabs)-->
-                            <div class="card">
+                            <!-- <div class="card">
                                 <div class="card-header">
                                     <h3 class="card-title">
                                         <i class="fas fa-chart-pie mr-1"></i>
@@ -515,27 +515,27 @@ $tv = $override->getCount('visit', 'visit_date', date('Y-m-d'));
                                                 <a class="nav-link" href="#sales-chart" data-toggle="tab">Donut</a>
                                             </li>
                                         </ul>
-                                    </div>
+                                    </div> -->
                                 </div><!-- /.card-header -->
-                                <div class="card-body">
-                                    <div class="tab-content p-0">
+                                <!-- <div class="card-body">
+                                    <div class="tab-content p-0"> -->
                                         <!-- Morris chart - Sales -->
-                                        <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;">
+                                        <!-- <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;">
                                             <canvas id="revenue-chart-canvas" height="300" style="height: 300px;"></canvas>
                                         </div>
                                         <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;">
                                             <canvas id="sales-chart-canvas" height="300" style="height: 300px;"></canvas>
-                                        </div>
-                                    </div>
+                                        </div> -->
+                                    <!-- </div> -->
                                 </div><!-- /.card-body -->
-                            </div>
+                            <!-- </div> -->
                             <!-- /.card -->
 
                             <!-- /.card -->
-                        </section>
+                        <!-- </section> -->
                         <!-- /.Left col -->
                         <!-- right col (We are only adding the ID to make the widgets sortable)-->
-                        <section class="col-lg-5 connectedSortable">
+                        <!-- <section class="col-lg-5 connectedSortable"> -->
 
                             <!-- Map card -->
                             <!-- <div class="card bg-gradient-primary">
@@ -585,17 +585,17 @@ $tv = $override->getCount('visit', 'visit_date', date('Y-m-d'));
 
 
                             <!-- Calendar -->
-                            <div class="card bg-gradient-success">
+                            <!-- <div class="card bg-gradient-success">
                                 <div class="card-header border-0">
 
                                     <h3 class="card-title">
                                         <i class="far fa-calendar-alt"></i>
                                         Calendar
-                                    </h3>
+                                    </h3> -->
                                     <!-- tools card -->
-                                    <div class="card-tools">
+                                    <!-- <div class="card-tools"> -->
                                         <!-- button with a dropdown -->
-                                        <div class="btn-group">
+                                        <!-- <div class="btn-group">
                                             <button type="button" class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown" data-offset="-52">
                                                 <i class="fas fa-bars"></i>
                                             </button>
@@ -612,20 +612,20 @@ $tv = $override->getCount('visit', 'visit_date', date('Y-m-d'));
                                         <button type="button" class="btn btn-success btn-sm" data-card-widget="remove">
                                             <i class="fas fa-times"></i>
                                         </button>
-                                    </div>
+                                    </div> -->
                                     <!-- /. tools -->
-                                </div>
+                                <!-- </div> -->
                                 <!-- /.card-header -->
-                                <div class="card-body pt-0">
+                                <!-- <div class="card-body pt-0"> -->
                                     <!--The calendar -->
-                                    <div id="calendar" style="width: 100%"></div>
-                                </div>
+                                    <!-- <div id="calendar" style="width: 100%"></div> -->
+                                <!-- </div> -->
                                 <!-- /.card-body -->
-                            </div>
+                            <!-- </div> -->
                             <!-- /.card -->
-                        </section>
+                        <!-- </section> -->
                         <!-- right col -->
-                    </div>
+                    <!-- </div> -->
                     <!-- /.row (main row) -->
                 </div><!-- /.container-fluid -->
             </section>
